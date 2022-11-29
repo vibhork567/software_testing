82f288279c12894e4e86d506de48eeeb0974923d
vibhork567 <47589095+vibhork567@users.noreply.github.com>
2022-11-29 15:32:48 +0530

    Delete bidding-frontend directory

--- bidding-frontend/src/services/UserService.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const BASE_URL = "http://localhost:9090/user";
-
-class UserService{
-     getUsers(){
-        return axios.get(BASE_URL);
-    }
-}
-export default new UserService();
